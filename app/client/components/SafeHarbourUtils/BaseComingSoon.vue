6b9f1234dc1156c019784861ba190cab799f189d
JasonVeljanoski-Portfolio <jasonveljanoski2020@outlook.com>
2020-10-29 19:51:15 +0800

    finished coming soon component

--- app/client/components/SafeHarbourUtils/BaseComingSoon.vue
+++ app/client/components/SafeHarbourUtils/BaseComingSoon.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="flexbox">
-      <h4>Coming Soon...</h4>
+      <div class="container">
+        <BaseAmmarAvatar />
+      </div>
+      <div class="text">
+         <h4>"Coming Soon..."</h4>
+      </div>
   </div>
 </template>
 
 <script>
+import BaseAmmarAvatar from '@/components/SafeHarbourUtils/BaseAmmarAvatar'
+
 export default {
-  name: 'ComingSoon'
+  name: 'ComingSoon',
+  components: {
+    BaseAmmarAvatar
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~static/css/main.styl'
+
+.container 
+  width 100%
+
+.text
+  width 100%
+  max-width 550px
 </style>
